5b394eda3849705c50751be253ceaa34b241cfb4
Mikaël H-Deschamps <mikael.hubert-deschamps@usherbrooke.ca>
2023-02-25 12:40:33 -0500

    Add routing to main page, interface canvas and basic struct

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HeaderLayout from "./HeaderLayout";
+import InterfaceA from "./pages/InterfaceA";
+import InterfaceB from "./pages/InterfaceB";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<HeaderLayout/>}>
+              <Route path={'interface-a'} element={<InterfaceA/>} />
+              <Route path={'interface-b'} element={<InterfaceB/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
-
-export default App;
