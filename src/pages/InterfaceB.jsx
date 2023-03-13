bce9ebd9be6cd44ec4679e84e5e94f066986d5dd
kebm2702 <mansourkb@hotmail.com>
2023-03-13 00:10:54 -0400

    interface B update
    The interface's layout has been updated according to the design wireframe

--- src/pages/InterfaceB.jsx
+++ src/pages/InterfaceB.jsx
@@ -3,13 +3,15 @@ import './InterfaceB.css';
 
 const cards = [  { title: 'Card 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },  { title: 'Card 2', content: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.' },  { title: 'Card 3', content: 'Integer ultrices aliquam tellus, ut condimentum massa tristique vel.' },];
 
-const images = [  { src: 'https://via.placeholder.com/450x150', alt: 'Placeholder image 1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },  { src: 'https://via.placeholder.com/450x150', alt: 'Placeholder image 2', text: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.' },  { src: 'https://via.placeholder.com/450x150', alt: 'Placeholder image 3', text: 'Integer ultrices aliquam tellus, ut condimentum massa tristique vel.' },];
+const images = [  { src: 'https://via.placeholder.com/450x150', alt: 'Placeholder image 1', title: 'Lorem ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },  { src: 'https://via.placeholder.com/450x150', alt: 'Placeholder image 2', title: 'Lorem ipsum', text: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.' },  { src: 'https://via.placeholder.com/450x150', alt: 'Placeholder image 3', title: 'Lorem ipsum', text: 'Integer ultrices aliquam tellus, ut condimentum massa tristique vel.' },];
 
 function InterfaceB() {
   return (
     <div>
-      <h1>Title</h1>
+      <div  className="header">
+      <h1 className="title">Title</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
       <div className="cards-container">
         {cards.map((card, index) => (
           <div key={card.title} className="card">
@@ -18,13 +20,14 @@ function InterfaceB() {
           </div>
         ))}
       </div>
-      <div className="images-container" style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="images-container">
         {images.map((image, index) => (
           <div className={index % 2 === 0 ? 'image' : 'image-reverse'} key={index}>
+            <img src={image.src} alt={image.alt} />
             <div>
+              <h2>{image.title}</h2>
               <p>{image.text}</p>
             </div>
-            <img src={image.src} alt={image.alt} style={{ width: 450, height: 150, objectFit: 'cover' }} />
           </div>
         ))}
       </div>
